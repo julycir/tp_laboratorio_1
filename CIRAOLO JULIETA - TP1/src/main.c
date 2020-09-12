aa90497b0ed769e1e96cb7ad62361a29c498a693
julycir <julycir@gmail.com>
2020-09-11 22:46:10 -0300

    actualizando

--- CIRAOLO JULIETA - TP1/src/main.c
+++ CIRAOLO JULIETA - TP1/src/main.c
@@ -4,30 +4,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "functions.h"
+#include "functions.h" //estas son las funciones que yo cree
 
 int main(void) {
 
+	setbuf(stdout,NULL); //para poder ver en la consola
     int opcion;
     int firstNumber;
     int secondNumber;
-    int additionResult;
+    /*int additionResult;
     int subtractionResult;
     float divisionResult;
-    int multiplicationResult;
+    int multiplicationResult;*/
     //int factorialResult;
 
     do
     {
-        printf("Elija una opcion: ");
-        printf("\n1. Ingresar primer operando: (A=%d)", firstNumber);
-        printf("\n2. Ingresar segundo operando: (B=%d)", secondNumber);
+    	//menu de opciones para el usuario
+        printf("1. Ingresar primer operando: ");
+        printf("\n2. Ingresar segundo operando: ");
         printf("\n3. Calcular operaciones");
         printf("\n4. Informar resultados");
         printf("\n5. Salir");
+        printf("\nElija una opcion: \n");
         scanf("%d", &opcion);
 
-        switch(opcion)
+       switch(opcion)
         {
             case 1: // se solicita ingreso del primer operando
                 printf("Ingrese un primer operando: ");
@@ -37,7 +39,7 @@ int main(void) {
             	printf("Ingrese un segundo operando: ");
             	scanf("%d",&secondNumber);
             	break;
-            case 3: // se calculan todas las operaciones
+            	/* case 3: // se calculan todas las operaciones
             	additionResult = AddNumbers(firstNumber, secondNumber);
             	subtractionResult = SubtractNumbers(firstNumber, secondNumber);
             	divisionResult = DivideNumbers(firstNumber, secondNumber);
@@ -50,7 +52,7 @@ int main(void) {
             	printf("c. El resultado de A/B es: %f\n", divisionResult);
             	printf("d. El resultado de A*B es: %d\n", multiplicationResult);
             	//printf("e. El factorial de A es: %d", factorialResult);
-            	break;
+            	break; */
             case 5:
                 printf("\nGracias por usar el programa...\n");
                 break;
